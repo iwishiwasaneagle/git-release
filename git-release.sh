cda1a26597cd58a7849b023b1e2bb3f1cb92a5b0
iwishiwasaneagle <jh.ewers@gmail.com>
2022-04-20 11:26:19 +0100

    fix: typo

--- git-release.sh
+++ git-release.sh
@@ -8,7 +8,7 @@ git release [options] <tagname>
 
 Options
     --verify       Run git hooks. Default skips. WARNING: Tags and that may need to be deleted if a hook is run and it fails the push/tag creation/commit/etc.
-    --no-skip-ci   Don't add a message to the commit to skip the pre-commit ci (only relevant if you are using pre-commit ci
+    --no-skip-ci   Don't add a message to the commit to skip the pre-commit ci (only relevant if you are using pre-commit ci)
 
 Tag options
     -m, --message <message>   Tag message (defaults to changelog)
